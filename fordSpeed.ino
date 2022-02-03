4baad5e862ffc878f230736e96a384bd8e4a99f7
Robotto <mark@moore.dk>
2022-02-03 15:20:55 +0100

    finished making code do things with SPI

--- fordSpeed.ino
+++ fordSpeed.ino
@@ -18,11 +18,11 @@ Copyleft 2022
 
 
 //Using hardware SPI pins:
-static unsigned int HV_ClockPin = 15; //SCLK
-static unsigned int HV_DataPin = 16;  //MOSI
-static unsigned int HV_LatchPin = 14;
-static unsigned int HV_BlankPin = 10;
-static unsigned int HV_PolarityPin = A0;
+static unsigned int HV_ClockPin = 15; //SCLK  - HV pin 28
+static unsigned int HV_DataPin = 16;  //MOSI - HV pin 32
+static unsigned int HV_LatchPin = 14; //HV pin 31
+static unsigned int HV_BlankPin = 10; //HV pin 33
+//static unsigned int HV_PolarityPin = A0;
 
 static unsigned int DCDC_EnablePin = 9;
 
@@ -30,49 +30,61 @@ static unsigned int DCDC_EnablePin = 9;
 void setup()
 {
     
-    pinMode(DCDC_EnablePin,OUTPUT);
-    digitalWrite(DCDC_EnablePin,LOW);
-    
     Serial.begin(9600);
     Serial1.begin(9600);
+    pinMode(DCDC_EnablePin,OUTPUT);
+    digitalWrite(DCDC_EnablePin,HIGH); //youtu.be/R-FxmoVM7X4
+
     SPI.begin();
 
     pinMode(HV_ClockPin,OUTPUT);
     pinMode(HV_DataPin,OUTPUT);
     pinMode(HV_LatchPin,OUTPUT);
     pinMode(HV_BlankPin,OUTPUT);
-    pinMode(HV_PolarityPin,OUTPUT);
+//    pinMode(HV_PolarityPin,OUTPUT);
 
 
     digitalWrite(HV_ClockPin,LOW);
     digitalWrite(HV_DataPin,LOW);
-    digitalWrite(HV_LatchPin,HIGH);
-    digitalWrite(HV_BlankPin,HIGH);
-    digitalWrite(HV_PolarityPin,LOW); //On means sink current, off means high voltage on.
+    digitalWrite(HV_LatchPin,LOW);
+    digitalWrite(HV_BlankPin,HIGH); 
+ //   digitalWrite(HV_PolarityPin,HIGH); //LOW: enabled pins source current, HIGH: Enabled pins sink current
 
 
-    SPI.beginTransaction(SPISettings(8000000, MSBFIRST, SPI_MODE1));
+    SPI.beginTransaction(SPISettings(2000000, MSBFIRST, SPI_MODE2));
 
-    delay(5000);
+    //delay(5000);
 
-    digitalWrite(DCDC_EnablePin,HIGH); //youtu.be/R-FxmoVM7X4
-
+    shiftOutBitPatternFromSpeed((uint8_t)255);
 }
 
 
+uint8_t rxByte = 0;
+uint8_t rxOld = 250;
 
 void loop()
 {
- uint8_t rxByte = 0;
- uint8_t rxOld = 255;
-
     if(Serial.available()) rxByte = Serial.read();
-    if(Serial1.available()) rxByte = Serial.read();
+//    if(Serial1.available()) rxByte = Serial1.read();
  
     if(rxByte != rxOld){
-        shiftOutBitPatternFromSpeed(rxByte);
-        rxOld=rxByte;        
+      if(rxByte==255)     {
+        digitalWrite(HV_BlankPin,LOW);  //Blank the outputs, and turn off DCDC
+        digitalWrite(DCDC_EnablePin,LOW);
+      }
+      else     
+      {
+        digitalWrite(HV_BlankPin,HIGH); 
+        digitalWrite(DCDC_EnablePin,HIGH);
+      }
+      shiftOutBitPatternFromSpeed(rxByte);
+      rxOld=rxByte;        
+        
+      while(Serial.available()) Serial.read();
+//      while(Serial1.available()) Serial1.read();
+      
     }
+
 }
 
 void shiftOutBitPatternFromSpeed(uint8_t speed){
@@ -91,25 +103,34 @@ void shiftOutBitPatternFromSpeed(uint8_t speed){
 
     speedOut = (uint32_t)Cout<<20 | (uint32_t)Xout<<10 | (uint32_t)Iout;
 
-    Serial.print("Speed in: "); Serial.print(speed,DEC); Serial.print(". Cout: "); Serial.print(Cout,BIN); Serial.print(", Xout: "); Serial.print(Xout,BIN); Serial.print(", Iout: "); Serial.println(Iout,BIN);
-    Serial.print("speedOut: "); Serial.println(speedOut,BIN);
+//    Serial.print("Speed in: "); Serial.println(speed,DEC); 
+//    Serial.print("Cout: "); Serial.print(Cout,BIN); Serial.print(", Xout: "); Serial.print(Xout,BIN); Serial.print(", Iout: "); Serial.println(Iout,BIN);
 
-/*
-    //make 4 bytes from the uint32_t, because shiftout won't eat more than one byte,
-    //so we have to feed it four times...
-    //also used for testing:
+
+    /*
+    make 4 bytes from the uint32_t, because SPI transfer won't eat more than one byte,
+    so we have to feed it four times...
+    also used for testing:
+    */
     uint8_t frame[4]={
         0xff&(speedOut>>24), //MSB
         0xff&(speedOut>>16), 
         0xff&(speedOut>>8), 
         0xff&speedOut        //LSB
     };
-*/
+
+//    Serial.print("speedOut: "); // Serial.println(speedOut,BIN);
 
     digitalWrite(HV_LatchPin,LOW);
-    //for(int i=0 ; i<4 ; i++){
-        //shiftOut(HV_DataPin,HV_ClockPin,MSBFIRST,frame[i]);
-    SPI.transfer(speedOut,4);
-    //}
+    SPI.transfer(&frame,4);
+/*
+    for(int i=0 ; i<4 ; i++){
+        SPI.transfer(frame[i]);
+        for(int j=8-String(frame[i],BIN).length();j>0;j--) Serial.print("0"); //pad with zeroes up to 8 bits per byte.
+        Serial.print(frame[i],BIN); Serial.print(" ");
+    }
+    Serial.println();
+*/
     digitalWrite(HV_LatchPin,HIGH);
+   
 }
